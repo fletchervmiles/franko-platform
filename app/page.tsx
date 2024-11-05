3775cc41e120caeac10253fdba5acaa39fbf54b0
fletchervmiles <98453784+fletchervmiles@users.noreply.github.com>
2024-11-05 16:39:48 +0700

    add

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <div>Hello World</div>
+      <div>Hi Worldly</div>
     </main>
   );
 }
