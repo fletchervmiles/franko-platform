a47d07c438ce9e0a1e7b3297ea4494087892edec
fletchervmiles <98453784+fletchervmiles@users.noreply.github.com>
2024-11-06 10:42:20 +0700

    auth-done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,42 +1,45 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
+import Header from "@/components/header";
 import { Providers } from "@/components/utilities/providers";
+import { createProfile, getProfileByUserId } from "@/db/queries/profiles-queries";
+import { ClerkProvider } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Franko",
-  description: "Customer Insights",
+  title: "Todo App",
+  description: "A full-stack template for a todo app."
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  const { userId } = await auth();
+
+  if (userId) {
+    const profile = await getProfileByUserId(userId);
+    if (!profile) {
+      await createProfile({ userId });
+    }
+  }
+
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Providers
-          attribute="class"
-          defaultTheme="dark"
-          disableTransitionOnChange
-        >
-          {children}
-        </Providers>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <Providers
+            attribute="class"
+            defaultTheme="dark"
+            disableTransitionOnChange
+          >
+            <Header />
+            {children}
+            <Toaster />
+          </Providers>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
